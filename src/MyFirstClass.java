e08cffe12c8b27a1edb8642b86b4909da8171894
zkzk8504 <zkzk8504@naver.com>
2017-10-20 10:53:46 +0900

    first commit

---
+++ src/MyFirstClass.java
@@ -0,0 +1,15 @@
+
+public class MyFirstClass {
+	public static void main(String[] args) {
+		// 데이터 타입
+		int i = 1;
+		short s = 1;
+		byte b = 1;
+		long l = 1;
+		float f = 1.1F;
+		double d = 1.1;
+		boolean bool = true;
+		char c = '중';
+		System.out.println();
+	}
+}
---
+++ src/OperatorEx.java
@@ -0,0 +1,50 @@
+
+public class OperatorEx {
+	public static void main(String[] args) {
+		// 산술연산자
+		System.out.println(5.2 + 3.1);
+		System.out.println(5.2 - 3.1);
+		System.out.println(5.2 * 3.1);
+		System.out.println(5.2 / 3.1);
+		System.out.println(5.2 % 3.1);
+				
+		// 단항연산자
+		int i = 1;
+		i++;
+		System.out.println(i);
+		i--;
+		System.out.println(i);
+		boolean b = true;
+		System.out.println(b);
+		System.out.println(!b);
+		
+		// 관계연산자
+		System.out.println("관계연산자");
+		System.out.println(3 == 3);
+		System.out.println(3 != 3);
+		System.out.println(3 > 3);
+		System.out.println(3 >= 3);
+		System.out.println(3 < 3);
+		System.out.println(3 <= 3);
+		
+		//조건 연산자 (논리연산자)
+		System.out.println("조건연산자 &&");
+		System.out.println(true && true);
+		System.out.println(false && false);
+		System.out.println(true && false);
+		System.out.println(false && true);
+		
+		System.out.println("조건연산자 ||");
+		System.out.println(true || true);
+		System.out.println(false || false);
+		System.out.println(true || false);
+		System.out.println(false || true);
+		
+		//비트연산자
+		System.out.println("조건연산자 &&");
+		System.out.println(5 & 3);
+		System.out.println(5 | 3);
+		System.out.println(5 << 1);
+		System.out.println(5 >> 1);
+	}
+}
